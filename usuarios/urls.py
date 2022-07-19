66f776190100f76f8ba6822b8bda6a7e7754576d
CarduCaldeira <carducaldeira@gmail.com>
2022-07-19 13:15:18 -0300

    segundo commit

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [path('cadastro', views.cadastro, name = 'cadastro'),
                path('cria/receita',views.cria_receita,name = 'cria_receita'),
                path('logout',views.logout,name = 'logout'),
                path('deleta/<int:receita_id>',views.deleta_receita,name = 'deleta_receita'),
+               path('edita/<int:receita_id>',views.edita_receita,name = 'edita_receita'),
+               path('atualiza_receita',views.atualiza_receita,name = 'atualiza_receita')
+
 ]
